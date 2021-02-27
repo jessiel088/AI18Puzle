ad984e4a0ae35cd6174de8a788542a20b9cae471
AlvaroGSV <18170244@itculiacan.edu.mx>
2021-02-26 22:10:11 -0700

    Se me habia olvidado subir el repositorio a github

---
+++ Puzle1-8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Puzle1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.generateGame();
+        }
+    }
+}
